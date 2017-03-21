349c73237667497c2580b416dde225f95514adde
kevimford <kevinmford@yahoo.com>
2017-03-21 15:32:13 -0700

    added modal for edit but it is currently not functioning

--- app.js
+++ app.js
@@ -18,16 +18,32 @@ $(document).ready(function(){
   }
 
   function renderList(){
+    console.log(groceryItems)
     var html = "";
     for(var i = 0; i < groceryItems.length; i++){
-      html += "<li data-index-number='" + i + "'>" + groceryItems[i].text + "<button class='btn btn-xs btn-danger deleteButton pull-right'>Delete</button></li>";
+      html += `<div class="row"><li`
+      if (groceryItems[i].checked){
+        html += " class= 'checked'";
+      }
+      html +=` data-index-number= ${i}> ${groceryItems[i].text}<div class="pull-right">` +
+      `<button class="btn btn-xs btn-primary fa fa-pencil edit" data-toggle="modal" data-target="#editBox"></button>`+
+      `<button class="btn btn-xs btn-info fa fa-check-circle-o check"></button>` +
+      `<button class="btn btn-xs btn-danger fa fa-times-circle delete"></button></div></li></div>`;
     }
     $("#theList").html(html);
+    // if item is checked create checked class
+    // lookup es6 interpolation
   }
 
-  $(document).on("click", ".deleteButton", function () {
-    var item = parseInt($(this).parent().attr("data-index-number"));
+  $(document).on("click", ".delete", function () {
+    var item = parseInt($(this).parent().parent().attr("data-index-number"));
     groceryItems.splice(item, 1);
+    renderList();
+  });
+
+   $(document).on("click", ".check", function () {
+    var item = parseInt($(this).parent().parent().attr("data-index-number"));
+    groceryItems[item].checked = !groceryItems[item].checked;
     renderList();
   });
 
